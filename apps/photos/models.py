3764e97b35674ca8e4cc82766ba0fdf0dcc8c844
elpintozo <ricardo.sr.lepe@gmail.com>
2020-07-28 14:35:42 -0400

    Ejemplo de uso de funciones callback en el modelo, en este caso pre_save

--- apps/photos/models.py
+++ apps/photos/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.text import slugify
+from django.db.models.signals import pre_save
 
 # Create your models here.
 class Photo(models.Model):
@@ -10,11 +11,28 @@ class Photo(models.Model):
     stock = models.IntegerField()
     slug = models.SlugField(null=False, blank=False, unique=True)
 
-    def save(self, *args, **kwargs):
-        ## haciendo uso de una funcion, notificamos que haga el slug a partir del titulo
-        self.slug = slugify(self.title)
-        super(Photo, self).save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     ## haciendo uso de una funcion, notificamos que haga el slug a partir del titulo
+    #     self.slug = slugify(self.title)
+    #     super(Photo, self).save(*args, **kwargs)
 
     ##la forma en como será visible le objeto al momento de ser pasado a string
     def __str__(self):
         return self.title
+
+def set_slug(sender, instance, *args, **kwargs):
+    """
+    Función que ayuda a generar el parametro slug de un object
+    Photo antes de ser creado.
+
+    ##se hace uso de una de las funciones -callback- para poder generar el slug antes de
+    ##crear una instancia de "Photo"
+
+    :param sender (Class): se indica la clase que se sobreescribirá un parametro
+    :param instance (Photo): instancia del objeto acrear
+    :return:
+    """
+    instance.slug = slugify(instance.title)
+
+##Se le indica al pre_save que función correr antes de hacer el save() y sobre que Objects
+pre_save.connect(set_slug, sender=Photo)
