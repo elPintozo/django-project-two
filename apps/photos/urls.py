a84ad8e07a3b134fa63cef3ee02db9a070228eaf
elpintozo <ricardo.sr.lepe@gmail.com>
2020-07-28 18:38:57 -0400

    ejemplo de como implementar busquedas con vistas basadas en clases

--- apps/photos/urls.py
+++ apps/photos/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PhotoDetailView
+from .views import PhotoDetailView, PhotoSearchListView
 
 urlpatterns = [
+    path('search', PhotoSearchListView.as_view(), name='product_search'),
     path('<slug:slug>', PhotoDetailView.as_view(), name='product_detail')
 ]
--- apps/photos/views.py
+++ apps/photos/views.py
@@ -44,5 +44,31 @@ class PhotoDetailView(DetailView):
         context['title'] = 'Details'
         return context
 
+class PhotoSearchListView(ListView):
+    """
+    Función que me ayuda a buscar un objecto
+    """
+    template_name = 'photos/search.html'
 
+    #Se sobreescribe la queryset de consulta
+    def get_queryset(self):
+        ##retorno los elementos con titulo similar al buscado por el usuario
+        return Photo.objects.filter(title__icontains=self.query())
+
+    ##se sobreescribe la funcion que obtiene el dato de consulta
+    def query(self):
+        #retorno el parametro ingresado para buscar
+        return self.request.GET.get('q')
+
+    ##sobreescribo la funcion que pasa los datos del view al template,
+    ##para asi poder agregar más data al tempalte
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        ##titulo para la pestaña
+        context['title'] = 'Search'
+        ##palabra clave a buscar
+        context['query'] = self.query()
+        #cantidad de elementos encontrados
+        context['count'] = context['photo_list'].count()
+        return context
 
