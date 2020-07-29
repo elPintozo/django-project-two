16a5eba6ae53564afc57f46dfd308051d4c9e1b8
elpintozo <ricardo.sr.lepe@gmail.com>
2020-07-29 19:00:27 -0400

    ejemplo de como evitar name duplicados en urls

--- apps/photos/urls.py
+++ apps/photos/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import PhotoDetailView, PhotoSearchListView
 
+##Al declarar el app_name, evitamos problemas de duplicado de nombre
+app_name = 'photos'
+
 urlpatterns = [
     path('search', PhotoSearchListView.as_view(), name='product_search'),
     path('<slug:slug>', PhotoDetailView.as_view(), name='product_detail')
