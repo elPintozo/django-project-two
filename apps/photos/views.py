4688a6a38424a5fc99bae46ac0defd96387af325
elpintozo <ricardo.sr.lepe@gmail.com>
2020-07-29 18:42:16 -0400

    ejemplo de como implementar consultas multiples usando Q

--- apps/photos/views.py
+++ apps/photos/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.db.models import Q
 from .models import Photo
 
 # Create your views here.
@@ -52,8 +53,9 @@ class PhotoSearchListView(ListView):
 
     #Se sobreescribe la queryset de consulta
     def get_queryset(self):
+        filters = Q(title__icontains=self.query()) | Q(category__title__icontains=self.query())
         ##retorno los elementos con titulo similar al buscado por el usuario
-        return Photo.objects.filter(title__icontains=self.query())
+        return Photo.objects.filter(filters)
 
     ##se sobreescribe la funcion que obtiene el dato de consulta
     def query(self):
