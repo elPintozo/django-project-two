a1e4c66718b3c3a2657d8842fe8f28d21e6af1ad
elpintozo <ricardo.sr.lepe@gmail.com>
2020-08-02 20:05:41 -0400

    se agrega implementación para agregar photos al carrito de compra

--- apps/carts/urls.py
+++ apps/carts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import cart
+from .views import cart, add
 
 app_name='carts'
 
 urlpatterns = [
-    path('', cart, name='cart')
+    path('', cart, name='cart'),
+    path('add', add, name='add')
 ]
--- apps/carts/views.py
+++ apps/carts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Cart
 from .utils import get_or_create_cart
+from apps.photos.models import Photo
 
 # Create your views here.
 def cart(request):
@@ -12,6 +13,28 @@ def cart(request):
     #diccionario con data para el template
     data = dict()
 
+    #Obtengo el carrito que se está usando
     cart = get_or_create_cart(request)
 
     return render(request, 'carts/cart.html', data)
+
+def add(request):
+    """
+    Función para agregar photos al carrito de compra
+    :param request (POST):
+    :return (template):
+    """
+    #diccionario con data para el template
+    data = dict()
+
+    # Obtengo el carrito que se está usando
+    cart = get_or_create_cart(request)
+
+    # Obtengo la photo del articulo que se quiere agregar al carrito
+    photo = Photo.objects.get(pk=request.POST.get('photo_id'))
+
+    #asocio la foto al carrito
+    cart.photos.add(photo)
+
+    data['photo']=photo
+    return render(request, 'carts/add.html', data)
