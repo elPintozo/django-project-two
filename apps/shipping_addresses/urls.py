92dd550c483c546c475e99262a07ee6278714265
elpintozo <ricardo.sr.lepe@gmail.com>
2020-08-23 18:14:30 -0400

    se hace uso de una vista basada en clases para poder eliminar una dirección seleccionada por el usuario

--- apps/shipping_addresses/urls.py
+++ apps/shipping_addresses/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import ShippingAddressListView, create
+from .views import ShippingAddressListView, create, ShippingAddressUpdateView, ShippingAddressDeleteView
 
 app_name = 'shipping_addresses'
 
 urlpatterns=[
     path('', ShippingAddressListView.as_view(), name='shipping_address'),
-    path('nuevo', create, name='create')
+    path('nuevo', create, name='create'),
+    path('editar/<int:pk>', ShippingAddressUpdateView.as_view(), name='update'),
+    path('eliminar/<int:pk>', ShippingAddressDeleteView.as_view(), name='delete')
 ]
--- apps/shipping_addresses/views.py
+++ apps/shipping_addresses/views.py
@@ -1,9 +1,16 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+
 from django.shortcuts import render
+from django.shortcuts import reverse
 from django.shortcuts import redirect
+from django.urls import reverse_lazy
+
 from django.views.generic import ListView
+from django.views.generic.edit import UpdateView
+from django.views.generic.edit import DeleteView
 
 from .models import Shipping_address
 from .forms import ShippingAddressForm
@@ -18,11 +25,96 @@ class ShippingAddressListView(LoginRequiredMixin, ListView):
     #variable que va de la mano con LoginRequiredMixin
     login_url = 'login'
 
+    #indico el model del cual debe conectar
     model = Shipping_address
+
+    #indico el template sobre cual actuar
     template_name = 'shipping_addresses/shipping_addresses.html'
 
     def get_queryset(self):
         return Shipping_address.objects.filter(user=self.request.user).order_by('-default')
+
+class ShippingAddressUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
+    """
+    Clase que me ayuda a poder cargar la vista para editar
+    la dirección de un usuario
+    """
+    # variable que va de la mano con LoginRequiredMixin
+    login_url = 'login'
+
+    # indico el model del cual debe conectar
+    model = Shipping_address
+
+    #indico el form del cual debe tomar para editar los campos
+    form_class = ShippingAddressForm
+
+    # indico el template sobre cual actuar
+    template_name = 'shipping_addresses/update.html'
+
+    #Hago uso de los message de Django para informar al usuario de que se logró editar
+    success_message="Dirección actualizada exitosamente"
+
+    def get_success_url(self):
+        """
+        Función que me ayuda a indicar a donde redireccionar
+        una vez se halla realizado las modificaciones a la
+        dirección
+        :return (URL):
+        """
+        return reverse('shipping_addresses:shipping_address')
+
+    def dispatch(self, request, *args, **kwargs):
+        """
+        Función que me ayuda a realizar validaciones al momento
+        de ejecutar la función
+        :param request (session):
+        :param args (dict):
+        :param kwargs (dict):
+        :return (redirect):
+        """
+        #valido si la dirección a editar le corresponde al usuario logueado
+        if request.user.id != self.get_object().user_id:
+            # si no le corresponde es redireccionado
+            return redirect('carts:cart')
+
+        return super(ShippingAddressUpdateView, self).dispatch(request, *args, **kwargs)
+
+class ShippingAddressDeleteView(LoginRequiredMixin, DeleteView):
+    """
+    Función que me ayuda a eliminar una dirección seleccionada por el usuario
+    """
+    # variable que va de la mano con LoginRequiredMixin
+    login_url = 'login'
+
+    # indico el model del cual debe conectar
+    model = Shipping_address
+
+    # indico el template sobre cual actuar
+    template_name = 'shipping_addresses/delete.html'
+
+    #indico donde debe ser redireccionado al momento de realizar la eliminacion
+    success_url= reverse_lazy('shipping_addresses:shipping_address')
+
+    def dispatch(self, request, *args, **kwargs):
+        """
+        Función que me ayuda a realizar validaciones al momento
+        de ejecutar la función
+        :param request (session):
+        :param args (dict):
+        :param kwargs (dict):
+        :return (redirect):
+        """
+        #valido que la dirección a eliminar no sea la por defecto
+        if self.get_object().default:
+            # si no le corresponde es redireccionado
+            return redirect('shipping_addresses:shipping_address')
+
+        # valido si la dirección a editar le corresponde al usuario logueado
+        if request.user.id != self.get_object().user_id:
+            # si no le corresponde es redireccionado
+            return redirect('carts:cart')
+
+        return super(ShippingAddressDeleteView, self).dispatch(request, *args, **kwargs)
 
 @login_required(login_url='login')
 def create(request):
